0906527ea66aedfcd00e7592d26aecf3bfcccaa1
vishal kumar <iamvishalnigam@gmail.com>
2025-08-24 11:27:31 +0530

    dbChanged_first

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,20 @@
-import mongoose from "mongoose";
+import { Sequelize } from "sequelize";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Connected');
-    } catch (error) {
-        console.log("error occured", error); 
-    }
-}
+  try {
+    const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+      dialect: "postgres", // you can also use 'mysql' | 'sqlite' | 'mariadb'
+      logging: false, // set true if you want SQL logs in console
+    });
+
+    await sequelize.authenticate();
+    console.log("PostgreSQL Connected ✅");
+
+    return sequelize; // return instance for models
+  } catch (error) {
+    console.error("❌ Error connecting to PostgreSQL:", error);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
